6b626e52deee527f1b4a7476cab59a9c283c2548
Gibdos <simon.yousefi@protonmail.com>
2022-02-23 19:25:26 +0100

    Finished exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+let shout = '';
+const repeatString = function(string, num) {
+    let i = 0;
+    if (num < 0) {
+        return "ERROR";
+    } else {
+    while (i < num) {
+        shout += string;
+        i++
+    }
+    let shoutOut = shout;
+    shout = ''
+    return shoutOut;
+}
 };
 
 // Do not edit below this line
