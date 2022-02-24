3894553212f06ceaf2f93e5ac8be8984d174501b
Gibdos <simon.yousefi@protonmail.com>
2022-02-24 08:27:07 +0100

    Finished exercise 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let stringReversed = reverseArray.join("")
+    return stringReversed
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(parameterArray, ...parameter) {
+    let arr = Array.from(parameterArray);
+    let forDeletion = parameter;
+    arr = arr.filter(item => !forDeletion.includes(item))
+    return arr;
 };
 
 // Do not edit below this line
