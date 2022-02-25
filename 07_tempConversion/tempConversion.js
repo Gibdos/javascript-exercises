839b48b27331f6b1ea5f7f66f5de8c4c0c815448
Gibdos <simon.yousefi@protonmail.com>
2022-02-25 14:46:55 +0100

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let deciRound1 = Math.round((((fahrenheit - 32) * (5 / 9)) * 1000)) / 1000;
+  let deciRound2 = Math.round(deciRound1);
+  let deciRound3 = (Math.round((deciRound2 - deciRound1) * 10) / 10)
+  let deciFinal = deciRound2 - deciRound3;
+  return deciFinal;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let deciRound1 = Math.round(((celsius * (9 / 5) + 32) * 1000)) / 1000;
+  let deciRound2 = Math.round(deciRound1);
+  let deciRound3 = (Math.round((deciRound2 - deciRound1) * 10) / 10)
+  let deciFinal = deciRound2 - deciRound3;
+  return deciFinal;
 };
 
 // Do not edit below this line
