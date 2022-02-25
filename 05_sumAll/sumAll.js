13aa80ad297f3086a9238452958144db0c28b52d
Gibdos <simon.yousefi@protonmail.com>
2022-02-25 13:52:55 +0100

    Finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,28 @@
-let arrNumbers = [];
-let totalSum = 0;
-
 const sumAll = function(firstInt, secondInt) {
-    for (i = firstInt; i <= secondInt; i++) {
-        arrNumbers.push(i);
-    }
-    for (i = 0; i <= arrNumbers.length; i++) {
-        totalSum = totalSum + i;
-    } return totalSum;
+    let arrNumbers = [];
+    let totalSum = 0;
+    
+    if (firstInt < 0 || secondInt < 0 || typeof firstInt != "number" || typeof secondInt != "number" ) {
+        return 'ERROR'
+    }else if (firstInt < secondInt) {
+        for (i = firstInt; i <= secondInt; i++) {
+            arrNumbers.push(i);
+        }
+        for (i = 0; i <= arrNumbers.length; i++) {
+            totalSum = totalSum + i;
+        }
+    } else if (firstInt > secondInt) {
+        for (i = secondInt; i <= firstInt; i++) {
+            arrNumbers.push(i);
+        }
+        for (i = 0; i <= arrNumbers.length; i++) {
+            totalSum = totalSum + i;
+        }
+    } else {
 
-    // let total = arrNumbers.reduce(( previousValue, currentValue ) => previousValue + currentValue,0)
-    // return total;
-};
+    }
+    return totalSum;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
