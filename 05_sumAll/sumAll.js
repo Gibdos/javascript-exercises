74d0f005b6ece2c283369850f909a033e9530f40
Gibdos <simon.yousefi@protonmail.com>
2022-02-25 12:50:41 +0100

    fuck if I know...

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+let arrNumbers = [];
+let totalSum = 0;
 
+const sumAll = function(firstInt, secondInt) {
+    for (i = firstInt; i <= secondInt; i++) {
+        arrNumbers.push(i);
+    }
+    for (i = 0; i <= arrNumbers.length; i++) {
+        totalSum = totalSum + i;
+    } return totalSum;
+
+    // let total = arrNumbers.reduce(( previousValue, currentValue ) => previousValue + currentValue,0)
+    // return total;
 };
 
 // Do not edit below this line
