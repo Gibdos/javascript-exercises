461b4f202b7b8af31a053140b859555f8b36bbe0
Gibdos <simon.yousefi@protonmail.com>
2022-02-26 11:55:20 +0100

    Finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	const regEx = /[^a-zA-Z0-9]/g; // RegEx to remove anything but characters and numbers
+	const cleanString = string.replaceAll(regEx, ""); // Remove all punctuation from string
 
+	function cleanUp(toClean) {
+		// Reverse the string
+		const testString = toClean;
+		// console.log(testString);
+		const splitString = testString.split(""); // Turn all seperate characters into an array
+		// console.log(splitString);
+		const reverseArr = splitString.reverse(); // Reverse the array
+		// console.log(reverseArr);
+		const joinedString = reverseArr.join(""); // Turn the reversed array into a string
+		// console.log(joinedString);
+		const finalString = joinedString.toUpperCase(); // Make the reversed string UPPERCASE
+		// console.log(finalString);
+		return finalString;
+	}
+
+	let finalString = cleanUp(cleanString); // Make a copy of the original string (without puncuation) and reverse it
+	let firstString = cleanString.toUpperCase(); // Make the original string (without puncuation) UPPERCASE
+	return finalString == firstString ? true : false; // Compare the reversed string with the original string (without punctuation)
 };
 
 // Do not edit below this line
