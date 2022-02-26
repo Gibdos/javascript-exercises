bedfd688d7450bf47950a37341b88ab0920ec963
Gibdos <simon.yousefi@protonmail.com>
2022-02-26 10:41:29 +0100

    finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,47 @@
-const add = function() {
-	
+const add = function (x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function ([...x]) {
+	if (x.length != 0) {
+		let sumUp = x.reduce(function (x, y) {
+			return x + y;
+		});
+		return sumUp;
+	} else {
+		return 0;
+	}
 };
 
-const multiply = function() {
-
+const multiply = function (x) {
+	let sumMult = x.reduce(function (x, y) {
+		return x * y;
+	});
+	return sumMult;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+	let fact = 1;
+	for (i = 1; i <= x; i++) {
+		fact = fact * i;
+	}
+	return fact;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
