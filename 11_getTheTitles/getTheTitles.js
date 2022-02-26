8af56f1b67cbfaa32934376d592ccede4cde0eb8
Gibdos <simon.yousefi@protonmail.com>
2022-02-26 15:28:25 +0100

    Finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+	const titles = new Array(0); // new empty array
+	obj.forEach(function getTitle(element) {
+		//for each entry in obj
+		titles.push(element.title); // add obj.title to new array
+	});
+	return titles; // return the new array
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
