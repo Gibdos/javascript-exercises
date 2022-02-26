5779cb2bd50bece2320d9ac28e6f232f66c506c0
Gibdos <simon.yousefi@protonmail.com>
2022-02-26 13:19:07 +0100

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	if (num < 0) {
+		return "OOPS";
+	} else {
+		var num1 = 0;
+		var num2 = 1;
+		var sum;
+		for (i = 1; i < num; i++) {
+			sum = num1 + num2; // 1, 2, 3,
+			num1 = num2; // 1, 1, 2,
+			num2 = sum; // 1, 2, 3,
+		}
+		return num2;
+	}
 };
 
 // Do not edit below this line
