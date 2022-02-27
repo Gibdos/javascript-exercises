9722a9384b4da943d27119886bcabb506444c98b
Gibdos <simon.yousefi@protonmail.com>
2022-02-27 06:16:37 +0100

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (obj) {
+	let currentYear = new Date().getFullYear();
+	obj.forEach((element) => {
+		if (element.yearOfDeath === undefined) {
+			element.yearOfDeath = currentYear;
+			element.age = element.yearOfDeath - element.yearOfBirth;
+		} else {
+			element.age = element.yearOfDeath - element.yearOfBirth;
+		}
+	});
+	obj.sort((ob1, ob2) => {
+		return ob1.age < ob2.age ? 1 : -1;
+	});
+	return obj[0];
 };
 
 // Do not edit below this line
